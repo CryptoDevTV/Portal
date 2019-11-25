94005caa557c8afa2c2b74287c58d24ed4392ca3
Tomasz Kowalczyk <tomek@kownet.info>
2019-11-25 23:05:05 +0100

    main page poc

---
+++ Portal/Portal.Web/Pages/Course.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Portal.Web.Pages
+{
+    public class CourseModel : PageModel
+    {
+        private readonly ILogger<PageModel> _logger;
+
+        public CourseModel(ILogger<PageModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public void OnGet(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
