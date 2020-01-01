80619456796c1e35d675acb444aa141f8160d949
Tomasz Kowalczyk <tomek@kownet.info>
2020-01-01 13:10:12 +0100

    ui changes

---
+++ Portal/Portal.Web/Pages/Newsletter.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portal.Web.Pages
+{
+    public class NewsletterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
